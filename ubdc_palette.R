49b0b73555218e939f382a06495df1fd92cb6fb8
David Henderson <daibhaidh.hen@gmail.com>
2017-10-06 19:28:11 +0100

    palette adjustments

--- ubdc_palette.R
+++ ubdc_palette.R
@@ -8,7 +8,7 @@ ubdc_palette <- palette_maker("ubdc_logo.jpg", number_of_colors = 32)
 #Repeating this brings different results order but in general there are 28 colours
 #identified although many of these are very alike. 
 
-ubdc_palette <- palette_maker("ubdc_logo.jpg", number_of_colors = 20)
+ubdc_palette <- palette_maker("ubdc_logo.jpg", number_of_colors = 15)
 
 #Much better option - will need to drop one colour (#FFFFFF) which is basically white
 
@@ -16,8 +16,8 @@ ubdc_palette <- ubdc_palette[ubdc_palette != "#FFFFFF"]
 
 #And now will rearrange manually in a better order
 
-ubdc_palette <- c("#F07329", "#35B14E", "#13AFD6", "#E9E826", "#D7509A", "#BCD032",
-                  "#866BAC", "#545A5D", "#2165AF", "#7A8082", "#E2D988", "#628DB7",
+ubdc_palette <- c("#13AFD6", "#E6E600", "#F07329", "#35B14E", "#D7509A", "#2165AF",
+                  "#BCD032","#866BAC", "#545A5D",  "#7A8082", "#E2D988", "#628DB7",
                   "#929B9A", "#93B8DA", "#31649B", "#FBF8D0", "#ACB2B4")
 
 #Dropped another 2 colours that were too "white". 
